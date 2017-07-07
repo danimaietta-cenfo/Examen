863386c8c46218560ae486a93ca6004a6c90f520
DESKTOP-GSV5HOI\danix <dmaiettae@ucenfotec.ac.cr>
2017-07-07 00:15:39 -1200

    first

---
+++ components/views/CameraComponent.js
@@ -0,0 +1,73 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  View,
+  Image,
+} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+import { Icon } from 'react-native-elements';
+import { getDatabase } from '../common/database';
+import Helper from '../common/helper';
+
+export default class CameraComponent extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        imagePath: '',
+        imageHeight: '',
+        imageWidth: ''
+      }
+   }
+
+   openImagePicker(){
+     var options = {
+       title: 'Select Avatar',
+       storageOptions: {
+         skipBackup: true,
+         path: 'images'
+       }
+     }
+     ImagePicker.showImagePicker(options, (response) => {
+       if(response.didCancel){
+         console.log('User cancelled image picker')
+       }else if(response.error){
+         console.log('Error'+response.error)
+       }else if(response.customButton){
+         console.log('User tapped custom button'+response.customButton)
+       }else{
+         this.setState({
+           imagePath: response.uri,
+           imageHeight: response.height,
+           imageWidth: response.width
+         })
+         this.addImage(response)
+       }
+     })
+   }
+
+   addImage(response){
+     let storageRef = getDatabase().storage.ref('daily/').put(response.uri);
+   }
+
+  render(){
+    return (
+        <View>
+            {this.state.imagePath ? <Image style={{width: 300, height: 300}} source={{uri: this.state.imagePath}} /> : null}
+            <Icon
+                  reverse
+                  name='camera-enhance'
+                  color='#517fa4'
+                  onPress={this.openImagePicker.bind(this)}
+              />
+        </View>
+  )}
+
+}
+
+module.export = CameraComponent;
